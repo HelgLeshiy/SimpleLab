c6d7df47a1c22f2ad0d7644fc6798cd9be000814
Zlvr <zilvertron@yandex.ru>
2017-04-11 01:35:33 +0300

    Add graphic SDL Interface

---
+++ GUI/main.cpp
@@ -0,0 +1,58 @@
+#include "hdr.h"
+
+int main( int *argc, char **argv )
+{
+	SDL_Init( SDL_INIT_EVERYTHING );
+
+	wnd = SDL_CreateWindow( "SimpleLab ver. 0.0.1_dev", 0, 0, WIDTH_SCR, HEIGH_SCR, 0 );
+	rnd = SDL_CreateRenderer( wnd, -1, SDL_RENDERER_SOFTWARE );
+
+	buttonsDraw( *rnd );
+
+	int pressedButtonID = -1;
+	do
+	{
+		pressedButtonID = butttonPush( pressedButtonID );
+		switch ( pressedButtonID )
+		{
+			case 0:
+			{
+
+			}
+			break;
+			case 1:
+			{
+
+			}
+			break;
+			case 2:
+			{
+
+			}
+			break;
+			case 3:
+			{
+
+			}
+			break;
+			case 4:
+			{
+
+			}
+			break;
+			case 5:
+			{
+
+			}
+			break;
+			case 6:
+
+				break;
+		}
+	}
+	while ( pressedButtonID != 0 );
+
+	SDL_Delay( 3500 );
+
+	return 0;
+}
