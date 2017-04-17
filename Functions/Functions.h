7d6740c7cd9a9798a9be898680b54c9951609350
Sikres <Sikres97@gmail.com>
2017-04-17 14:15:06 +0300

    More Files

--- Functions/Functions.h
+++ Functions/Functions.h
@@ -1,1 +1,15 @@
-typedef double Matrix//vector<vector<complex<double>>> Matrix
+#pragma once
+#include "../Parser/Namescope.h"
+#include<string>
+#include<vector>
+#include<stdexcept>
+Matrix f_sin(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_cos(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_tan(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_ctan(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_sinh(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_cosh(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_tanh(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_ctanh(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_lengthStr(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_random(Namescope*, const std::vector<std::shared_ptr<Value>>&);
