c742002d645cf5c739c918ea7c419434c2aebf91
Sikres <Sikres97@gmail.com>
2017-05-30 10:25:31 +0300

    ...

--- Functions/Functions.h
+++ Functions/Functions.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "../Parser/Namescope.h"
 #include<string>
+#include<math.h>
 #include<vector>
 #include<stdexcept>
 Matrix f_sin(Namescope*, const std::vector<std::shared_ptr<Value>>&);
@@ -11,5 +12,11 @@ Matrix f_sinh(Namescope*, const std::vector<std::shared_ptr<Value>>&);
 Matrix f_cosh(Namescope*, const std::vector<std::shared_ptr<Value>>&);
 Matrix f_tanh(Namescope*, const std::vector<std::shared_ptr<Value>>&);
 Matrix f_ctanh(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_logn(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_log(Namescope*, const std::vector<std::shared_ptr<Value>>&);
 Matrix f_lengthStr(Namescope*, const std::vector<std::shared_ptr<Value>>&);
 Matrix f_random(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_integral(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_differencial(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_minFunc(Namescope*, const std::vector<std::shared_ptr<Value>>&);
+Matrix f_maxFunc(Namescope*, const std::vector<std::shared_ptr<Value>>&);
